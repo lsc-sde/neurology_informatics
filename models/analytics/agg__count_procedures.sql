fbcc1a1d9a36dabc84f0615eee717855d80528ab
Fran B <frances_biggin@yahoo.co.uk>
2024-06-26 11:50:11 +0100

    updated procedures

--- models/analytics/agg__count_procedures.sql
+++ models/analytics/agg__count_procedures.sql
@@ -1,11 +1,13 @@
 select 
     procedure_source_value, 
     concept_name,
+    specialty_source_value,
     count(person_id) as number_of_patients 
 from {{ ref('stg__procedures') }} 
 group by 
     procedure_source_value,
-    concept_name
+    concept_name,
+    specialty_source_value
 order by 
     number_of_patients desc OFFSET 0 rows
     
--- models/data_characterisation/stg__procedures.sql
+++ models/data_characterisation/stg__procedures.sql
@@ -4,10 +4,15 @@ select
     po.procedure_concept_id, 
     po.procedure_date, 
     po.procedure_source_value,
+    po.provider_id,
+    p.provider_name,
+    p.specialty_source_value,
     c.concept_name
 from  {{ source('omop', 'procedure_occurrence') }} as po
 inner join {{ ref('stg__distinct_patients') }} as do 
     on po.person_id = do.person_id
 inner join {{ source('vocab', 'concept') }} as c
-  on po.procedure_concept_id = c.concept_id
+    on po.procedure_concept_id = c.concept_id
+inner join {{ source('omop', 'provider') }} as p
+    on p.provider_id = po.provider_id 
 where po.procedure_date >=  {{ var('clinic_visit_start_date') }}
