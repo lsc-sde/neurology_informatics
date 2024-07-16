7ed31ff0a1cbe19ea443de25e15f61e13dc6fa8c
Fran B <frances_biggin@yahoo.co.uk>
2024-07-16 10:03:56 +0100

    Added sql to id patients who only have OP appointment
    Also amended the procedures sql to identify only procedures related to outpatient appointments

---
+++ models/data_characterisation/stg__outpatient_only.sql
@@ -0,0 +1,3 @@
+select person_id
+from {{ ref('stg__distinct_patients') }}
+where person_id not in (select person_id from {{ ref('stg__inpatient_episodes') }})
--- models/data_characterisation/stg__procedures.sql
+++ models/data_characterisation/stg__procedures.sql
@@ -1,16 +1,17 @@
 select 
-    po.procedure_occurrence_id, 
     po.person_id, 
-    po.procedure_concept_id, 
     po.procedure_date, 
     po.procedure_source_value,
-    po.provider_id,
-    p.provider_name,
-    p.specialty_source_value,
-    c.concept_name
+    p.provider_name as procedure_provider,
+    p.specialty_source_value as procedure_specialty,
+    c.concept_name as procedure_concept_name,
+    vo.visit_occurrence_id,
+    vo.visit_start_date as outpatient_appointment_date,
+    vo.provider_name as outpatient_provider,
+    vo.specialty_source_value as outpatient_specialty
 from  {{ source('omop', 'procedure_occurrence') }} as po
-inner join {{ ref('stg__distinct_patients') }} as do 
-    on po.person_id = do.person_id
+inner join {{ ref('stg__visit_occurrence_outpatients') }} as vo 
+    on po.visit_occurrence_id = vo.visit_occurrence_id
 inner join {{ source('vocab', 'concept') }} as c
     on po.procedure_concept_id = c.concept_id
 inner join {{ source('omop', 'provider') }} as p
