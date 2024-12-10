769190dbe67fe5ace784740966de84a2cf6e2770
Fran B <frances_biggin@yahoo.co.uk>
2024-12-10 11:26:00 +0000

    Updated dates again

--- models/black hole counts/cohort.sql
+++ models/black hole counts/cohort.sql
@@ -4,8 +4,8 @@ inner join {{ source('omop', 'provider') }} as pr
   on vo.provider_id = pr.provider_id
 where
   vo.visit_source_value = 'OP'
-  and vo.visit_start_date >= '2022-05-01'
-  and vo.visit_start_date <= '2023-04-30'
+  and vo.visit_start_date >= '2022-04-01'
+  and vo.visit_start_date <= '2023-03-31'
   and vo.provider_id in (
     select provider_id from {{ ref('Current_Neurology_providers') }}
   )
